4c880087b1cb27217fc3950fecb84e997888e01c
Anderson Maia <andersonmaiacode@gmail.com>
2023-03-23 14:34:08 -0300

    Finished the JS code!

--- script.js
+++ script.js
@@ -15,28 +15,72 @@ let chosenWord = "";
 let newDash;
 let newWordLetter;
 let wordLettersArr = [];
+let dummyParts = [headImg, bodyImg, legLImg, legRImg, armRImg, armLImg];
+let clicksCounter = 0;
 
 randomWord();
 constructWord();
+dummyHandler();
 
 reloadBtn.addEventListener("click", reset);
 
 for (let l of letters) {
+  l.disabled = false;
+
   l.addEventListener("click", () => {
+    l.classList.remove("hover");
+    l.disabled = true;
+    l.style.color = "crimson";
+    clicksCounter += 1;
+
     for (c of chosenWord) {
       if (c === l.innerText) {
         for (let wordL of wordLettersArr) {
           if (wordL.innerText == c) {
             wordL.classList.add("visible");
-            wordL.classList.remove("hidden");
-            console.log(wordL);
+            wordL.classList.remove("invisible");
+            l.style.color = "darkgreen";
+            clicksCounter -= 1;
           }
         }
-      } else {
-        console.log(c + "  no match");
       }
     }
+
+    console.log(clicksCounter);
+    dummyHandler();
   });
+}
+
+function dummyHandler() {
+  if (clicksCounter === 0) {
+    for (let part of dummyParts) {
+      part.classList.add("invisible");
+    }
+  } else if (clicksCounter === 1) {
+    dummyParts[3].classList.add("visible");
+  } else if (clicksCounter === 2) {
+    dummyParts[2].classList.add("visible");
+  } else if (clicksCounter === 3) {
+    dummyParts[1].classList.add("visible");
+  } else if (clicksCounter === 4) {
+    dummyParts[4].classList.add("visible");
+  } else if (clicksCounter === 5) {
+    dummyParts[5].classList.add("visible");
+  } else if (clicksCounter === 6) {
+    dummyParts[0].classList.add("visible");
+    alert("You lost!");
+  }
+
+  let lettersCorrect = [];
+  for (let wordL of wordLettersArr) {
+    if (wordL.classList.contains("visible")) {
+      lettersCorrect.push(wordL);
+    }
+  }
+
+  if (lettersCorrect.length === wordLettersArr.length && clicksCounter < 6) {
+    alert("You Won!");
+  }
 }
 
 function randomWord() {
@@ -66,17 +110,13 @@ function constructWord() {
     wordDashes.append(newDash);
 
     newWordLetter = document.createElement("h1");
-    newWordLetter.classList.add("word-letter", "hidden");
+    newWordLetter.classList.add("word-letter", "invisible");
     newWordLetter.innerHTML = l;
     wordLetters.append(newWordLetter);
     wordLettersArr.push(newWordLetter);
 
     console.log(wordLetters);
   }
-}
-
-function checkLetter() {
-  return "";
 }
 
 function reset() {
